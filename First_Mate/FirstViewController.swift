d9010dc55c17c218993ee5e66ec7c00f78c74a9b
mrjacobs55 <rmjacobs@optonline.net>
2016-02-17 17:13:39 -0500

    Initial Commit

---
+++ First_Mate/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  First_Mate
+//
+//  Created by Mitchell Jacobs on 2/17/16.
+//  Copyright © 2016 TZHSCompSciClub. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
