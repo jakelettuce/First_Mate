8faafb1330addba4d1272a852300ed63bd74dcb0
mrjacobs55 <rmjacobs@optonline.net>
2016-02-17 17:29:00 -0500

    Added Third and Fourth View

---
+++ First_Mate/FourthViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FourthViewController.swift
+//  First_Mate
+//
+//  Created by Mitchell Jacobs on 2/17/16.
+//  Copyright © 2016 TZHSCompSciClub. All rights reserved.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
