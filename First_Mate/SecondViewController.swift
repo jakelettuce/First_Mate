c33a224399c873b074e7ada52ebba94d7b8a311d
mrjacobs55 <rmjacobs@optonline.net>
2016-02-18 17:28:29 -0500

    Added Map Icon and Schedule Icon

--- First_Mate/SecondViewController.swift
+++ First_Mate/SecondViewController.swift
@@ -13,7 +13,9 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
